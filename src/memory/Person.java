0d8c69ff5a44e34210f1f015452624e10d53c55c
admin <valery.muhanin@yandex.ru>
2024-10-27 21:22:49 +0300

    Pamyat Java

---
+++ src/memory/Person.java
@@ -0,0 +1,21 @@
+package memory;
+
+public class Person {
+
+    String name;
+
+    String surname;
+
+    public Person (String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString () {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
